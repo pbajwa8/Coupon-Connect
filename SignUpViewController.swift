fdb3f577d5c00021be5eac4416a375ed51646fb4
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-08-14 22:01:01 -0400

    added view controllers

---
+++ SignUpViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpViewController.swift
+//  Coupon Connect
+//
+//  Created by Parambir Bajwa on 8/14/15.
+//  Copyright (c) 2015 Parambir Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
