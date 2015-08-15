a652aa34aa9a7d9efaa4ef59d5376c23397232c6
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-08-14 21:01:08 -0400

    First commit

---
+++ Coupon Connect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coupon Connect
+//
+//  Created by Parambir Bajwa on 8/14/15.
+//  Copyright (c) 2015 Parambir Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
