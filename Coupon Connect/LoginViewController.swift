052f8e69aeaf85362f22907ba6ab5e8fa9fc152c
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-08-15 08:15:27 -0400

    login works, coupons save to db

--- Coupon Connect/LoginViewController.swift
+++ Coupon Connect/LoginViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
 
     override func viewDidLoad() {
         self.navigationController?.navigationBarHidden = true
@@ -21,6 +23,38 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func signIn(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(self.usernameField.text, password:self.passwordField.text) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                var currentUser: PFUser = PFUser.currentUser()!
+                var userId: String! = currentUser["userId"] as! String
+                println(userId)
+                var couponsUrl = "http://service.sandbox.cw.cm?API_KEY=54090b6a70faf7b1cbe27305e6&method=get_wallet_info&u=" + userId + "&OUTPUT=json"
+                
+                println(couponsUrl)
+                
+                let getCouponsUrl = NSURL(string: couponsUrl)
+                
+                let getCoupons = NSURLSession.sharedSession().dataTaskWithURL(getCouponsUrl!) {(data, response, error) in
+                    let json = JSON(data: data)
+                    if json[0]["accepted_businesses"].string != nil {
+                        currentUser["coupon1"] = json[0]["accepted_businesses"].string as String!
+                    }
+                    if json[1]["accepted_businesses"].string != nil {
+                       currentUser["coupon2"] = json[1]["accepted_businesses"].string as String!
+                    }
+                    currentUser.save()
+                    println("coupons saved")
+                }
+                getCoupons.resume()
+            } else {
+                println(error)
+            }
+        }
+    }
+    
     
 
     /*
