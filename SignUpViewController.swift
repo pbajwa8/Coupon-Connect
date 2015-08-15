0633e90e7c2113f7c10398d85704d0df30ad311a
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-08-15 02:05:31 -0400

    parse signup + api calls

--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var ageField: UITextField!
+    @IBOutlet var genderField: UITextField!
+    @IBOutlet var zipcodeField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +29,57 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func signUp(sender: AnyObject) {
+        
+        var userAccessToken = ""
+        var userId = ""
+        
+        var tokenUrl = "http://auth.sandbox.cw.cm/login?API_KEY=54090b6a70faf7b1cbe27305e6&login_service=couponwallet&password=" + self.passwordField.text + "&username=" + self.usernameField.text
+        
+        let getTokenUrl = NSURL(string: tokenUrl)
+        
+        let getToken = NSURLSession.sharedSession().dataTaskWithURL(getTokenUrl!) {(data, response, error) in
+            if (error != nil) {
+                println(error)
+                println("THIS IS AN ERROR")
+            } else {
+                let json = JSON(data: data)
+                userAccessToken = json["access_token"].string as String!
+                println(userAccessToken)
+                var idUrl = "http://service.sandbox.cw.cm?API_KEY=54090b6a70faf7b1cbe27305e6&method=get_user_by_token&access_token=" + userAccessToken + "&OUTPUT=json"
+                
+                let getUserIdUrl = NSURL(string: idUrl)
+                
+                let getUserId = NSURLSession.sharedSession().dataTaskWithURL(getUserIdUrl!) {(data, response, error) in
+                    let json = JSON(data: data)
+                    userId = json["u"].string as String!
+                    var user = PFUser()
+                    user.username = self.usernameField.text
+                    user.password = self.passwordField.text
+                    user["age"] = self.ageField.text
+                    user["gender"] = self.genderField.text
+                    user["zipcode"] = self.zipcodeField.text
+                    user["userId"] = userId
+                    
+                    user.signUpInBackgroundWithBlock {
+                        (succeeded: Bool, error: NSError?) -> Void in
+                        if let error = error {
+                            let errorString = error.userInfo?["error"] as? NSString
+                            println(errorString)
+                        } else {
+                            println("User Created")
+                        }
+                }
+                
+                
+            }
+                getUserId.resume()
+        }
+        
+
+    }
+    getToken.resume()
+    
 
     /*
     // MARK: - Navigation
@@ -33,4 +91,5 @@ class SignUpViewController: UIViewController {
     }
     */
 
-}
+    }
+ }
