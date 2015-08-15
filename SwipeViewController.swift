2cb34c753564db5ad2b20a75b9ad7f0dd887f142
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-08-15 12:07:31 -0400

    saves matches to separate parse class

--- SwipeViewController.swift
+++ SwipeViewController.swift
@@ -10,11 +10,90 @@ import UIKit
 
 class SwipeViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = true
+        var matchedUsers: [String] = []
+        
+        var currentUser: PFUser = PFUser.currentUser()!
+        var currentUserGender: String! = currentUser["gender"] as! String
+        var currentUserCoupon1: String! = currentUser["coupon1"] as! String
+        var currentUserCoupon2: String! = currentUser["coupon2"] as! String
+        var currentUserUsername: String! = currentUser["username"] as! String
+        
+        var requestedGender = ""
+        
+        if currentUserGender == "M" {
+            requestedGender = "F"
+        } else if currentUserGender == "F" {
+            requestedGender = "M"
+        }
+        
+        var query = PFUser.query()
+        query!.whereKey("gender", equalTo:requestedGender)
+        
+        query!.findObjectsInBackgroundWithBlock( { (NSArray objects, NSError error) in
+            if error == nil {
+                if let objects = objects {
+                    for object in objects {
+                        if object.objectForKey("coupon1") as! String == currentUserCoupon2 {
+                            var matches = PFObject(className:"matches")
+                            matches["currentUser"] = currentUserUsername
+                            matches["matchedUser"] = object.objectForKey("username") as! String
+                            matches["age"] = object.objectForKey("age") as! String
+                            matches["matchedCoupon"] = currentUserCoupon2
+                            matches["photo"] = object.objectForKey("photo")
+                            matches.saveInBackgroundWithBlock {
+                                (success: Bool, error: NSError?) -> Void in
+                                if (success) {
+                                   print("saved")
+                                } else {
+                                    print("fuck this shit")
+                                }
+                            }
+                        } else if object.objectForKey("coupon2") as! String == currentUserCoupon2 {
+                            var matches = PFObject(className:"matches")
+                            matches["currentUser"] = currentUserUsername
+                            matches["matchedUser"] = object.objectForKey("username") as! String
+                            matches["age"] = object.objectForKey("age") as! String
+                            matches["photo"] = object.objectForKey("photo")
+                            matches["matchedCoupon"] = currentUserCoupon2
+                            matches.saveInBackgroundWithBlock {
+                                (success: Bool, error: NSError?) -> Void in
+                                if (success) {
+                                    print("saved")
+                                } else {
+                                    print("fuck this shit")
+                                }
+                            }
 
+                        } else if object.objectForKey("coupon1") as! String == currentUserCoupon1 {
+                            var matches = PFObject(className:"matches")
+                            matches["currentUser"] = currentUserUsername
+                            matches["matchedUser"] = object.objectForKey("username") as! String
+                            matches["age"] = object.objectForKey("age") as! String
+                            matches["photo"] = object.objectForKey("photo")
+                            matches["matchedCoupon"] = currentUserCoupon1
+                            matches.saveInBackgroundWithBlock {
+                                (success: Bool, error: NSError?) -> Void in
+                                if (success) {
+                                    print("saved")
+                                } else {
+                                    print("fuck this shit")
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+        })
+        
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
